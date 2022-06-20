2d714fd78c8438162fe9b69bf9bc5c354344e9e0
f.mousa <f.mousa@tecno-tech.co>
2022-06-20 16:51:18 +0200

    add

--- src/components/test.vue
+++ src/components/test.vue
@@ -2,20 +2,33 @@
     <div>
         <h1>{{Message}}</h1>
         <h1>{{Message2}}</h1>
+
+        <div class="post" v-for="(post,id) in posts" :key="id">
+            <h3>{{post.title}}</h3>
+            <p>{{post.body}}</p>
+        </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
     name: 'AppTest',
     data(){
         return{
             Message : 'Hello Guys from test component',
-            Message2 : 'أهلا بكم يا أصدقاء'
+            Message2 : 'أهلا بكم يا أصدقاء',
+            posts:[]
         }
     },
     created(){
-        alert('Created')
+        // alert('Created')
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(res=>{
+            this.posts = res.data
+        }).catch(e=>{
+            console.log(e)
+        })
     },
     beforeCreate() {
         alert('beforeCreate')
